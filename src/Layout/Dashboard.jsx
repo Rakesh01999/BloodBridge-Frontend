fc4d9daab63c95d9a957fa2eaeedc8b32ec2fe22
nafis200 <nafisahamed14@gmail.com>
2024-09-26 13:12:37 +0600

    donation

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { NavLink, Navigate, Outlet} from "react-router-dom";
+import { MdBloodtype } from "react-icons/md";
+import { FaHouseUser } from "react-icons/fa";
 
 const Dashboard = () => {
     return (
-        <div>
-            <h3 className='text-center font-bold text-3xl'> This Is Dashboard</h3>
+       
+            <div className="flex">
+            {/* side bar */}
+            <div className="w-64 min-h-screen bg-blue-600 text-white">
+            <ul className="menu p-4">
+            <li><NavLink to='/dashboard/donar'><MdBloodtype></MdBloodtype>Donation blood</NavLink> </li>
+            <li><NavLink to='/dashboard/history'><FaHouseUser></FaHouseUser> Donation History</NavLink> </li>
+            <li><NavLink to='/dashboard/requestblood'><MdBloodtype></MdBloodtype>request blood</NavLink> </li>
+            <li><NavLink to='/dashboard/requesthistory'><FaHouseUser></FaHouseUser>request history</NavLink> </li>
+            
+            </ul>
+           
+            </div>
+            {/* dashboard content */}
+            <div className="flex-1 p-8">
+                <Outlet></Outlet>
+            </div>
         </div>
     );
 };
---
+++ src/Pages/Dashboard/Donationblood/Requestblood.jsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react';
+
+const Requestblood = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Requestblood;
