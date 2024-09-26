9f2fb1da26f1a3f1ad4bd6d462cfee998dcc5ff8
nafis200 <nafisahamed14@gmail.com>
2024-09-27 01:12:58 +0600

    well done

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -11,9 +11,9 @@ const Dashboard = () => {
             <div className="md:w-64 w-64 min-h-screen bg-blue-600 text-white ">
             <ul className="menu p-8 lg:w-[280px]">
             <li><NavLink to='/dashboard/donar'><MdBloodtype></MdBloodtype>Donation blood</NavLink> </li>
-            <li><NavLink to='/dashboard/history'><FaHouseUser></FaHouseUser> Donation History</NavLink> </li>
+            {/* <li><NavLink to='/dashboard/history'><FaHouseUser></FaHouseUser> Donation History</NavLink> </li> */}
             <li><NavLink to='/dashboard/requestblood'><MdBloodtype></MdBloodtype>request blood</NavLink> </li>
-            <li><NavLink to='/dashboard/requesthistory'><FaHouseUser></FaHouseUser>request history</NavLink> </li>
+            <li><NavLink to='/dashboard/requesthistory'><FaHouseUser></FaHouseUser>Details of history</NavLink> </li>
             
             </ul>
            
